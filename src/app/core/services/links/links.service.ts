a725e091738f87dce32f537ad3c57a0ca637c3a6
baronrafael <baronrafael@hotmail.com>
2022-01-21 17:31:07 -0400

    link service and interface

--- src/app/core/services/links/links.service.ts
+++ src/app/core/services/links/links.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Link } from 'src/app/shared/interfaces/link/link';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LinksService {
 
-  constructor() { }
+  linksUrl!: string;
+
+  constructor(private httpClient: HttpClient) {
+    this.linksUrl = 'links';
+  }
+
+  create(link: Link) {
+    return this.httpClient.post(`${environment.apiUrl}${this.linksUrl}`, link);
+  }
+
+  list() {
+    return this.httpClient.get<Link[]>(`${environment.apiUrl}${this.linksUrl}`);
+  }
+
+  delete(linkId: number) {
+    return this.httpClient.delete(`${environment.apiUrl}${this.linksUrl}/${linkId}`);
+  }
+
 }
