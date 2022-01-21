93c4c6197e2d97823f5aff3964717e433ae75d7c
baronrafael <baronrafael@hotmail.com>
2022-01-21 17:24:08 -0400

    auth service and interfaces

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { LoginCredentials } from 'src/app/shared/interfaces/auth/login-credentials/login-credentials';
+import { RegisterCredentials } from 'src/app/shared/interfaces/auth/register-credentials/register-credentials';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  loginUrl!: string;
+  registerUrl!: string;
+
+  constructor(private httpClient: HttpClient) {
+    this.loginUrl = 'login';
+    this.registerUrl = 'register';
+  }
+
+  login(credentials: LoginCredentials) {
+    return this.httpClient.post(`${environment.apiUrl}${this.loginUrl}`, credentials)
+  }
+
+  register(credentials: RegisterCredentials) {
+    return this.httpClient.post(`${environment.apiUrl}${this.registerUrl}`, credentials)
+  }
+
 }
