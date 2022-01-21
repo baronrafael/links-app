5616ceee103f47140d34d37cb4c83f984f526ff7
baronrafael <baronrafael@hotmail.com>
2022-01-21 17:30:44 -0400

    enviroment route fix

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../../environments/environment';
+import { environment } from 'src/environments/environment';
 import { LoginCredentials } from 'src/app/shared/interfaces/auth/login-credentials/login-credentials';
 import { RegisterCredentials } from 'src/app/shared/interfaces/auth/register-credentials/register-credentials';
 
