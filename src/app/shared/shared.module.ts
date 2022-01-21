6e52dde8aa700d54000ec46051ab86d50684e569
baronrafael <baronrafael@hotmail.com>
2022-01-21 19:52:39 -0400

    profile card and link card

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
+import { ProfileCardComponent } from './components/profile-card/profile-card.component';
+import { LinkCardComponent } from './components/link-card/link-card.component';
 
 const COMPONENTS = [
-  HeaderComponent
+  HeaderComponent,
+  ProfileCardComponent,
+  LinkCardComponent
 ];
 
 const MODULES = [
