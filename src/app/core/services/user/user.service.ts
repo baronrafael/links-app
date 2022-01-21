e48537b4622fcf786ad8504c3916543f2d965cee
baronrafael <baronrafael@hotmail.com>
2022-01-21 17:30:57 -0400

    user service and interface

--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  userUrl!: string;
+
+  constructor(private httpClient: HttpClient) {
+    this.userUrl = 'user';
+  }
+
+  get(userId: number) {
+    return this.httpClient.get(`${environment.apiUrl}${this.userUrl}/${userId}`);
+  }
+
 }
