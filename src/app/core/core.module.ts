d9a64b5dfc35afa9ff7afb42eb25136f6c894b06
baronrafael <baronrafael@hotmail.com>
2022-01-22 07:43:56 -0400

    auth interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthInterceptor } from './interceptors/auth/auth.interceptor';
 
 const MODULES = [
   CommonModule,
@@ -17,5 +18,12 @@ const MODULES = [
   exports: [
     ...MODULES,
   ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+  ]
 })
 export class CoreModule { }
