a274a1f031e8b4199cacec72fc4186e90a38a368
baronrafael <baronrafael@hotmail.com>
2022-01-22 09:17:21 -0400

    behavior subject for user

--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from 'src/app/shared/interfaces/user/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
+  private user$ = new BehaviorSubject<User | undefined>(undefined);
+
+  getUser(): Observable<User | undefined> {
+    return this.user$.asObservable();
+  }
+  
+  setUser(user: User | undefined) {
+    this.user$.next(user);
+  }
 
   userUrl!: string;
 
