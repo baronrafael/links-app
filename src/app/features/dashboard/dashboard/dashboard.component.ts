c9dfb802358cf2c863e31f67051534daa5f9f0ef
baronrafael <baronrafael@hotmail.com>
2022-01-22 09:18:10 -0400

    get user logic in dashboard

--- src/app/features/dashboard/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NotificationService } from 'src/app/core/services/notification/notification.service';
+import { UserService } from 'src/app/core/services/user/user.service';
+import { User } from 'src/app/shared/interfaces/user/user';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  user!: User;
+
+  UserDataSubscription!: Subscription;
+  userSubscription!: Subscription;
+  subscriptionArray: Subscription[] = [];
+
+  constructor(
+    private userService: UserService,
+    private notificationService: NotificationService
+  ) { }
 
   ngOnInit(): void {
+    this.getUserData();
+  }
+
+  getUserData() {
+    this.UserDataSubscription = this.userService.getUser()
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          if(res) {
+            this.user = res;
+          }
+          else {
+            this.getUser();
+          }
+        },
+        error: (err) => {
+          //console.error(err);
+          this.notificationService.errorMessage('userError');
+        }
+      });
+    this.subscriptionArray.push(this.UserDataSubscription);
+  }
+
+  getUser() {
+    this.userSubscription = this.userService.get()
+      .subscribe({
+        next: (res) => {
+          //console.log(res);
+          this.user = res as User;
+          this.userService.setUser(this.user);
+        },
+        error: (err) => {
+          //console.error(err);
+          this.notificationService.errorMessage('userError');
+        }
+      });
+    this.subscriptionArray.push(this.userSubscription);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionArray.forEach((subscription) => {
+      if (subscription) {
+        subscription.unsubscribe();
+      }
+    });
   }
 
 }
