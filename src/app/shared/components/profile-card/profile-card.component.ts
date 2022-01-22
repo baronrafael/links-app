e0b61613fd7005c32f9cc83873465f7b683bc384
baronrafael <baronrafael@hotmail.com>
2022-01-22 09:21:48 -0400

    integrate profile card

--- src/app/shared/components/profile-card/profile-card.component.ts
+++ src/app/shared/components/profile-card/profile-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/app/core/services/user/user.service';
+import { User } from '../../interfaces/user/user';
 
 @Component({
   selector: 'app-profile-card',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileCardComponent implements OnInit {
 
-  constructor() { }
+  $user!: Observable<User | undefined>;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getUserLogged();
+  }
+
+  getUserLogged() {
+    this.$user = this.userService.getUser();
   }
 
 }
