0f089f023713f4bdb770d45e67f9a647e17504b1
baronrafael <baronrafael@hotmail.com>
2022-01-21 20:54:39 -0400

    add link card component

---
+++ src/app/shared/components/add-link-card/add-link-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-link-card',
+  templateUrl: './add-link-card.component.html',
+  styleUrls: ['./add-link-card.component.scss']
+})
+export class AddLinkCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
 import { ProfileCardComponent } from './components/profile-card/profile-card.component';
 import { LinkCardComponent } from './components/link-card/link-card.component';
+import { AddLinkCardComponent } from './components/add-link-card/add-link-card.component';
 
 const COMPONENTS = [
   HeaderComponent,
   ProfileCardComponent,
+  AddLinkCardComponent,
   LinkCardComponent
 ];
 
