6ed74868a39632fed752f20754c4ec00104a28b0
baronrafael <baronrafael@hotmail.com>
2022-01-24 12:46:48 -0400

    simulate link list

--- src/app/core/services/links/links.service.ts
+++ src/app/core/services/links/links.service.ts
@@ -2,11 +2,37 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Link } from 'src/app/shared/interfaces/link/link';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LinksService {
+
+  links = [
+    {
+      "id": "1",
+      "createdAt": "2021-03-18T15:11:43.458Z",
+      "url": "https://daphne.com",
+      "name": "rodger.name"
+    },
+    {
+      "id": "2",
+      "createdAt": "2021-03-18T18:01:24.837Z",
+      "url": "http://ferne.biz",
+      "name": "german.name"
+    }
+  ];
+
+  private links$ = new BehaviorSubject<Link[] | undefined>(this.links);
+
+  getLinks(): Observable<Link[] | undefined> {
+    return this.links$.asObservable();
+  }
+  
+  setLinks(links: Link[] | undefined) {
+    this.links$.next(links);
+  }
 
   linksUrl!: string;
 
@@ -22,7 +48,7 @@ export class LinksService {
     return this.httpClient.get(`${environment.apiUrl}${this.linksUrl}`);
   }
 
-  delete(linkId: number) {
+  delete(linkId: string) {
     return this.httpClient.delete(`${environment.apiUrl}${this.linksUrl}/${linkId}`);
   }
 
